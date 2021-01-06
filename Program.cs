7f32d4ce3d49854af08b512d7a885594ccbc3099
NurgulFirat <43298890+NurgulFirat@users.noreply.github.com>
2021-01-06 23:51:41 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceUygulama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bilgisayar B1 = new Bilgisayar();
+           // B1.id = 1;  //değer girmek istemiyorum, uygulamanın bana bu alanı set edilebilir şekilde bırakmasını da istemiyorum.. ÇÖZÜM => 
+            B1.marka = "Lenovo";
+            B1.model = "z50";
+            B1.islemci = "I5";
+            B1.alisFiyat = -1;
+            B1.satisFiyat = 1600M;
+            B1.kampanyaFiyat = 1000M;
+            B1.barkod = "1234567890";
+
+            SanalDatabase.yeniUrunEkle(B1);
+
+            Bilgisayar B2 = new Bilgisayar();
+            B2.marka = "Lenovo";
+            B2.model = "z50";
+            B2.islemci = "I5";
+            B2.alisFiyat = -1;
+            B2.satisFiyat = 1600M;
+            B2.kampanyaFiyat = 1000M;
+            B2.barkod = "1234567890";
+
+            SanalDatabase.yeniUrunEkle(B2);
+
+
+        }
+    }
+}
---
+++ Urun.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceUygulama
+{
+    public class Urun : baseClass
+    {
+        public string marka { get; set; }   //* belitmiş olan markalar içerisinde ürün eklenebilsin
+        public string model { get; set; }
+
+        private decimal _alisFiyat;
+        public decimal alisFiyat    // 0'dan küçük olmamalı
+        {
+            get
+            {
+                return _alisFiyat;
+            }
+            set
+            {
+                if (alisFiyat <= 0)
+                {
+                    Console.WriteLine("Alış fiyatı 0'dan küçük veya eşit olamaz.");
+                }
+                else
+                {
+                    _alisFiyat = value;
+                }
+            }
+        }
+
+        private decimal _satisFiyat;
+        public decimal satisFiyat   //alisFiyat'ından küçük olamaz
+        {
+            get
+            {
+                return _satisFiyat;
+            }
+            set
+            {
+                if (value < _alisFiyat)
+                {
+                    Console.WriteLine("Satış fiyatı alış fiyatından küçük olamaz");
+                }
+                else
+                {
+                    _satisFiyat = satisFiyat;
+                }
+            }
+        }
+
+        private decimal _kampanyaFiyat;
+        public decimal kampanyaFiyat    //0'dan küçük olamaz
+        {
+            get
+            {
+                return _kampanyaFiyat;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    Console.WriteLine("Kampanya fiyatı 0'dan küçük olamaz.");
+                }
+                else
+                {
+                    _kampanyaFiyat = value;
+                }
+            }
+        }
+
+
+    }
+}
